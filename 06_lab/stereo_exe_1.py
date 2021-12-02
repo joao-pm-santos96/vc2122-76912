1acbac3b0b0eb2b83af67b1986d02c68149f6104
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2021-12-02 19:35:42 +0000

    add code

---
+++ 06_lab/stereo_exe_1.py
@@ -0,0 +1,98 @@
+# ***********************************************************************************
+# Name:           chessboard.py
+# Revision:
+# Date:           28-10-2019
+# Author:         Paulo Dias
+# Comments:       ChessBoard Tracking
+#
+# images         left1.jpg->left19.jpg
+# Revision:
+# ***********************************************************************************
+import numpy as np
+import cv2
+import glob
+
+# Board Size
+board_h = 9
+board_w = 6
+
+def  FindAndDisplayChessboard(img):
+    # Find the chess board corners
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, corners = cv2.findChessboardCorners(gray, (board_w,board_h),None)
+
+    # If found, display image with corners
+    if ret == True:
+        img = cv2.drawChessboardCorners(img, (board_w, board_h), corners, ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(100)
+
+    return ret, corners
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((board_w*board_h,3), np.float32)
+objp[:,:2] = np.mgrid[0:board_w,0:board_h].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+obj_points = [] # 3d point in real world space
+left_corners = []
+right_corners = []
+
+img_size = None
+
+# Read images
+images = sorted(glob.glob('.//images//left*.jpg'))
+
+for fname in images:
+    img = cv2.imread(fname)
+
+    if img_size is None:
+        img_size = img.shape
+
+    ret, corners = FindAndDisplayChessboard(img)
+    if ret == True:
+        obj_points.append(objp)
+        left_corners.append(corners)
+
+images = sorted(glob.glob('.//images//right*.jpg'))
+
+for fname in images:
+    img = cv2.imread(fname)
+    ret, corners = FindAndDisplayChessboard(img)
+    if ret == True:
+        # objpoints.append(objp)
+        right_corners.append(corners)
+
+
+# print('Right corners')
+# print(right_corners)
+
+# print('Left corners')
+# print(left_corners)
+
+# print('Object points')
+# print(obj_points)
+
+# Stereo calibration
+ret, cam_matrix_right, dist_coeffs_right, cam_matrix_left, dist_coeffs_left, R, T, E, F = cv2.stereoCalibrate(obj_points, right_corners, left_corners, None, None, None, None, img_size[1::-1], flags=cv2.CALIB_SAME_FOCAL_LENGTH)
+
+print(cam_matrix_right)
+print(cam_matrix_left)
+
+np.savez('stereoParams.npz',
+    intrinsics1=cam_matrix_right,
+    distortion1=dist_coeffs_right,
+    intrinsics2=cam_matrix_left,
+    distortion2=dist_coeffs_left,
+    R=R,
+    T=T,
+    E=E,
+    F=F)
+
+
+
+
+
+cv2.waitKey(-1)
+cv2.destroyAllWindows()
---
+++ 06_lab/stereo_exe_3.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+***DESCRIPTION***
+"""
+
+"""
+IMPORTS
+"""
+import numpy as np
+import cv2
+import glob
+from functools import partial
+
+"""
+METADATA
+"""
+__author__ = 'Joao Santos'
+__copyright__ = 'Copyright December2021'
+__credits__ = ['Joao Santos']
+__version__ = '1.0.0'
+__maintainer__ = 'Joao Santos'
+__email__ = 'joao.pm.santos96@gmail.com'
+__status__ = 'Production'
+# __license__ = 'GPL'
+
+"""
+TODO
+"""
+
+"""
+CLASS DEFINITIONS
+"""
+
+"""
+FUNCTIONS DEFINITIONS
+"""
+def mouse_handler(event, x, y, flags, params, F, right_image = None, left_image = None):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        p = np.asarray([x,y])
+        epilineR = cv2.computeCorrespondEpilines(p.reshape(-1,1,2), 1, F)
+        epilineR = epilineR.reshape(-1, 3)[0]
+
+        color = np.random.randint(0, 255, 3).tolist()
+        x0 = int(0)
+        y0 = int((-epilineR[2] - x0 * epilineR[0]) / epilineR[1])
+
+        x1 = int(1000)
+        y1 = int((-epilineR[2] - x1 * epilineR[0]) / epilineR[1])
+
+        if left_image is not None:
+            cv2.line(left_image, (x0, y0), (x1, y1), color, thickness=4)
+            cv2.imshow(left_window, left_image)
+
+        if right_image is not None:
+            cv2.line(right_image, (x0, y0), (x1, y1), color, thickness=4)
+            cv2.imshow(right_window, left_image)
+
+"""
+MAIN
+"""
+# Board Size
+board_h = 9
+board_w = 6
+
+left_window = 'Left Undist'
+right_window = 'Right Undist'
+
+if __name__ == '__main__':
+    
+    stereo_params = np.load('stereoParams.npz')
+    images_right = sorted(glob.glob('.//images//right*.jpg'))
+    images_left = sorted(glob.glob('.//images//left*.jpg'))
+
+    right_undist = cv2.undistort(cv2.imread(images_right[0]), stereo_params['intrinsics1'], stereo_params['distortion1'])
+
+    left_undist = cv2.undistort(cv2.imread(images_left[0]), stereo_params['intrinsics2'], stereo_params['distortion2'])
+
+    # cv2.imshow('Right Original', cv2.imread(images_right[0]))
+    # cv2.imshow('Left Original', cv2.imread(images_left[0]))
+
+    cv2.imshow(right_window, right_undist)
+    cv2.setMouseCallback('Right Undist', partial(mouse_handler, F=stereo_params['F'], left_image = left_undist))
+    
+    cv2.imshow(left_window, left_undist)
+    cv2.setMouseCallback('Left Undist', partial(mouse_handler, F=stereo_params['F'], right_image = right_undist))
+
+    
+    cv2.waitKey(0)
+
+    
